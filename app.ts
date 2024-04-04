48aee25c820b69e9206aca909b03e77ba8992733
maamirkh <maamirkhan1983@gmail.com>
2024-04-05 01:46:44 +0500

    updated

---
+++ app.ts
@@ -0,0 +1,6 @@
+#! usr/bin/env node
+// Calculates the absolute difference between -5 and 5
+const difference: number = Math.abs(-5 - 5);
+
+console.log(difference); // Outputs: 10
+// This line shows the absolute value of the difference between -5 and 5, ignoring the sign.
